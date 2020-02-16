3889b8f5f12c9d85be5092e151c17b45a2185eff
Misael dos Santos <47726159+misaelbs@users.noreply.github.com>
2020-02-16 11:58:32 -0300

    fix constants

--- utils/constants.js
+++ utils/constants.js
@@ -3,8 +3,5 @@ import {StatusBar} from "react-native";
 export default {
     statusBarHeight: StatusBar.currentHeight,
     emptyComponent: () => null,
-    emptyFn: () => {},
-    android: {
-        package: __DEV__? "host.exp.exponent": "br.com.inmeta.app"
-    }
+    emptyFn: () => {}
 }
